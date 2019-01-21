ac6ab0bb07579cb095bb1c8afcdc7339483159fb
igorgilew <ig.gilew2013@gmail.com>
2019-01-21 13:33:41 +0500

    Тест WCF

---
+++ WCFMyServiceLibrary/IMyService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+namespace WCFMyServiceLibrary
+{
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        string Method1(string x);
+        [OperationContract]
+        string Method2(string x);
+    }  
+}
---
+++ WCFMyServiceLibrary/MyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFMyServiceLibrary
+{
+    public class MyService: IMyService
+    {
+        public string Method1(string x)
+        {
+            return $"1 You entered: {x}===1";
+        }
+        public string Method2(string x)
+        {
+            //$-интерполяция строк вместо string.format
+            return $"2 You entered: {x}===2";
+        }
+    }
+}
